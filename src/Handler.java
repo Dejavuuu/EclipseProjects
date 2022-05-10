1b129935e9d0136ec805c79c771044e508ae7ba9
chris <chris@christophersair.attlocal.net>
2022-05-10 02:07:49 -0500

    Added notes and Handler class

--- src/Handler.java
+++ src/Handler.java
@@ -1,5 +1,32 @@
+import java.awt.Graphics;
+import java.util.LinkedList;
 
 public class Handler {
+	
+	//because we dont know how many game objects we'll have, create LinkedList
+	LinkedList<GameObject> object = new LinkedList<GameObject>();//import LinkedList [shortcut: <cmd><shift><o>]
+	
+	public void tick() { //updates all game objects
+		
+		for(int i = 0; i < object.size(); i++) {
+			GameObject tempObject = object.get(i);
+			
+			tempObject.tick();
+			
+			
+		}
+		
+	}
+	//renders all game objects
+	public void render(Graphics g) { //import Graphics [shortcut: <cmd><shift><o>]
+		for(int i = 0; i < object.size(); i++) {
+			GameObject tempObject = object.get(i);
+			
+			tempObject.render(g);
+		
+		}
+		
+	}
 	
 	
 
